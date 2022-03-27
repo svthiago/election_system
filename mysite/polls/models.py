acd3750959d557a279201f67f0172b73060c677d
Thiago Vilela <thiago.sv9@gmail.com>
2022-03-27 00:12:14 -0300

    Model type fix

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Question(models.Model):
-    question_text = models.CharField(max_lenght=200)
+    question_text = models.CharField(max_length=200)
     pub_date = models.DateField('date published')
 
 
